9ade4c07612a55e002fe1a6b63b280f465a5bf22
Maboal <maboal@floridauniversitaria.es>
2023-01-20 21:39:07 +0000

    Implementacion de JsonResponse en la ruta "/"  con info de tablas y sus relaciones

---
+++ src/Controller/MainController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_main')]
+    public function index(): JsonResponse
+    {
+        
+        // Json en formato string.
+        $json = '{
+            "tablas": [
+                {
+                    "name": "empresas",
+                    "asociada": {
+                        "1" : "pedidos"
+                    }
+                },
+                {
+                    "name": "facturas",
+                    "asociada": {
+                        "1" : "pedidos"
+                    }
+                },
+                {
+                    "name": "pedidos",
+                    "asociada": {
+                        "1" : "empresas",
+                        "2" : "facturas"
+                    }
+                },
+                {
+                    "name": "lineaspedidos",
+                    "asociada": {
+                        "1" : "pedidos",
+                        "2" : "productos"
+                    }
+                },
+                {
+                    "name": "productos",
+                    "asociada": {
+                        "1" : "lineaspedidos",
+                        "2" : "almacenes",
+                        "3" : "stock"
+                    }
+                },
+                {
+                    "name": "almacenes",
+                    "asociada": {
+                        "1" : "productos",
+                        "2" : "stock"
+                    }
+                },
+                {
+                    "name": "stock",
+                    "asociada": {
+                        "1" : "productos",
+                        "2" : "almacenes"  
+                    }
+                }
+            ]
+        }';
+        // Transformar string json a array mediante json_decode
+        // json_decode -> Takes a JSON encoded string and converts it into a PHP value.
+        $data = json_decode($json, true);
+        // Transformar array $data que contiene json codificado a objeto json.
+        return $this->json($data);
+    }
+}
